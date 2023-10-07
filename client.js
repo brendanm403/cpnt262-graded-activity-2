c609d27b2ddfa1309872bd00290529116708cc5a
Brendan Malycky <bmalycky@hotmail.com>
2023-10-07 13:56:48 -0600

    rendered initial values

---
+++ client.js
@@ -0,0 +1,12 @@
+// before string variable //
+const initialString = "This is the unmodified string"
+
+//before number variable //
+let initialNumber = 0;
+
+// render original values in html "before" section //
+let target = document.querySelector("#string");
+target.innerHTML = initialString;
+target = document.querySelector("#number");
+target.innerHTML = initialNumber;
+
