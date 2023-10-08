1c544ab043059c719bd1c011876bccaf66b48e84
Brendan Malycky <bmalycky@hotmail.com>
2023-10-07 18:05:58 -0600

    rendered variables in after section

--- client.js
+++ client.js
@@ -10,3 +10,34 @@ target.innerHTML = initialString;
 target = document.querySelector("#number");
 target.innerHTML = initialNumber;
 
+// string methods rendered in after section //
+
+// select target element //
+target = document.querySelector("#modified-string-1");
+// get string length, render in html //
+target.innerHTML = initialString.length;
+// select target element //
+target = document.querySelector("#modified-string-2");
+// replace word in string, render in html //
+target.innerHTML = initialString.replace("unmodified", "altered");
+// select target element //
+target = document.querySelector("#modified-string-3");
+// concatenate with another string, render in html //
+target.innerHTML = initialString + ". Or is it?";
+
+// number methods //
+
+// select target element //
+target = document.querySelector("#modified-number-1");
+// increment by 5, render in html //
+target.innerHTML = initialNumber += 5;
+// select target element //
+target = document.querySelector("#modified-number-2");
+// check in an integer, render in html //
+target.innerHTML = Number.isInteger(initialNumber);
+// select target element //
+target = document.querySelector("#modified-number-3");
+// reassigned variable //
+initialNumber = 5.25456;
+// render in html with 2 decimal places //
+target.innerHTML = initialNumber.toFixed(2);
